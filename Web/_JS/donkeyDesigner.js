a73a634c0512e48c29e84be93cdada970932b9bb
Sphrases <deraikoben@gmail.com>
2017-07-22 22:35:05 +0200

    Export and some more GUI stuff #6
    #keepTheDonkin

--- Web/_JS/donkeyDesigner.js
+++ Web/_JS/donkeyDesigner.js
@@ -5,6 +5,10 @@ donkeyCanvas.setWidth(700);
 donkeyCanvas.setHeight(400);
 
 
+$('#dummyButton1').click(function (e) {
+    console.log(donkeyCanvas.getActiveObject().get('type'));
+});
+
 $('#addRectangleButton').click(function (e) {
     var rect = new fabric.Rect({
         width: 100, height: 100, fill: 'blue', left: 50, top: 50
@@ -19,6 +23,11 @@ $('#addCircleButton').click(function (e) {
     });
     donkeyCanvas.add(circ); // add object
 
+});
+
+
+$('#exportButton').click(function (e) {
+    exportToFile();
 });
 
 
@@ -37,3 +46,71 @@ $('#removeElementButton').click(function (e) {
     }
 });
 
+
+function exportToFile() {
+    if (donkeyCanvas.getObjects().length != 0) {
+        donkeyCanvas._activeObject = null;
+        var objs = donkeyCanvas.getObjects().map(function (o) {
+            return o.set('active', true);
+        });
+
+
+        var group = new fabric.Group(objs, {
+            originX: 'center',
+            originY: 'center'
+        });
+
+        donkeyCanvas._activeObject = null;
+        donkeyCanvas.setActiveGroup(group.setCoords()).renderAll();
+        var allObjects = donkeyCanvas.getActiveGroup().getObjects();
+
+        var exportArray = [["__--This is a test Export--__"]];
+
+
+        for (var i = 0; i < allObjects.length; i++) {
+            exportArray.push(getShapeType(allObjects[i]));
+        }
+        //exportArray done
+        console.log(exportArray);
+        //now to txt file
+
+        var csvContent = "data:text/plain;charset=utf-8,";
+        exportArray.forEach(function(infoArray, index){
+            var dataString = infoArray.join(",");
+            csvContent += index < exportArray.length ? dataString+ "\n" : dataString;
+
+        });
+
+        var encodedUri = encodeURI(csvContent);
+        var link = document.createElement("a");
+        link.setAttribute("href", encodedUri);
+        link.setAttribute("download", "export .txt");
+        document.body.appendChild(link); // Required for FF
+
+        link.click(); // This will download the data file named "my_data.csv".
+
+    }
+}
+
+
+function getShapeType(object) {
+    var objectArray = [];
+    switch (object.get('type')) {
+        case('circle'):
+            objectArray = ['circle', Math.floor(object.left), Math.floor(object.top), object.radius];
+            break;
+        case('rect'):
+            objectArray = ['rect', Math.floor(object.left), Math.floor(object.top), object.width, object.height];
+            break;
+        case('triangle'):
+            objectArray = ['triangle', Math.floor(object.left), Math.floor(object.top)];
+            break;
+        case('image'):
+            objectArray = ['image', Math.floor(object.left), Math.floor(object.top)];
+            break;
+        default:
+
+            break;
+    }
+    return objectArray;
+}
