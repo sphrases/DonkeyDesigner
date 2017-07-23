561b45b86f69ffc61891d0d9f525610a73777cd6
Sphrases <deraikoben@gmail.com>
2017-07-23 03:02:35 +0200

    moar stuff...
    ...cant list everything!

--- Web/_JS/donkeyDesigner.js
+++ Web/_JS/donkeyDesigner.js
@@ -4,25 +4,46 @@ var donkeyCanvas = new fabric.Canvas('donkeyCanvas', {
 donkeyCanvas.setWidth(700);
 donkeyCanvas.setHeight(400);
 
+var globalParams = {
+    borderColor: '#42c4c7',
+    cornerColor: '#42c4c7',
+    cornerSize: 8,
+    transparentCorners: false,
+    hasRotatingPoint: false
+}
+var elemendAdded = false;
+var dummyText = "This is a placeholder text, to see how your text-field looks";
 
-$('#dummyButton1').click(function (e) {
-    console.log(donkeyCanvas.getActiveObject().get('type'));
-});
 
 $('#addRectangleButton').click(function (e) {
     var rect = new fabric.Rect({
-        width: 100, height: 100, fill: 'blue', left: 50, top: 50
+        width: 100, height: 100, fill: getRandomColor(), left: 50, top: 50
     });
+    rect.set(globalParams);
     donkeyCanvas.add(rect); // add object
 
 });
 
 $('#addCircleButton').click(function (e) {
     var circ = new fabric.Circle({
-        radius: 50, fill: 'red', left: 50, top: 50
+        radius: 50, fill: getRandomColor(), left: 50, top: 50
     });
+    circ.set(globalParams);
     donkeyCanvas.add(circ); // add object
 
+});
+
+
+$('#addTextButton').click(function (e) {
+    var textBox = new fabric.Textbox(dummyText, {
+        fontFamily: 'arial',
+        fontSize: 27,
+        fill: getRandomColor(),
+        left: 40,
+        top: 40,
+    });
+    textBox.set(globalParams);
+    donkeyCanvas.add(textBox);
 });
 
 
@@ -47,7 +68,43 @@ $('#removeElementButton').click(function (e) {
 });
 
 
+donkeyCanvas.on('mouse:down', function (param) {
+    var object = param.target;
+    if (object != null) {
+        switch (object.get('type')) {
+            case('circle'):
+                //showCircleControls();
+                break;
+            case('rect'):
+                break;
+            case('triangle'):
+                break;
+            case('image'):
+                break;
+            case('i-text'):
+                break;
+            case('text'):
+                break;
+            case('textbox'):
+                showTextControls(object);
+                $('#elementSettingsDiv').visibility = true;
+                elemendAdded = true;
+                break;
+            default:
+
+                break;
+        }
+    } else {
+        $('#elementSettingsDiv').empty();
+        $('#elementSettingsDiv').visibility = false;
+        elemendAdded = false;
+    }
+
+});
+
+
 function exportToFile() {
+    donkeyCanvas._activeObject = null;
     if (donkeyCanvas.getObjects().length != 0) {
         donkeyCanvas._activeObject = null;
         var objs = donkeyCanvas.getObjects().map(function (o) {
@@ -64,7 +121,7 @@ function exportToFile() {
         donkeyCanvas.setActiveGroup(group.setCoords()).renderAll();
         var allObjects = donkeyCanvas.getActiveGroup().getObjects();
 
-        var exportArray = [["__--This is a test Export--__"]];
+        var exportArray = [["#Export"]];
 
 
         for (var i = 0; i < allObjects.length; i++) {
@@ -75,16 +132,16 @@ function exportToFile() {
         //now to txt file
 
         var csvContent = "data:text/plain;charset=utf-8,";
-        exportArray.forEach(function(infoArray, index){
+        exportArray.forEach(function (infoArray, index) {
             var dataString = infoArray.join(",");
-            csvContent += index < exportArray.length ? dataString+ "\n" : dataString;
+            csvContent += index < exportArray.length ? dataString + "\n" : dataString;
 
         });
 
         var encodedUri = encodeURI(csvContent);
         var link = document.createElement("a");
         link.setAttribute("href", encodedUri);
-        link.setAttribute("download", "export .txt");
+        link.setAttribute("download", "export.txt");
         document.body.appendChild(link); // Required for FF
 
         link.click(); // This will download the data file named "my_data.csv".
@@ -108,9 +165,64 @@ function getShapeType(object) {
         case('image'):
             objectArray = ['image', Math.floor(object.left), Math.floor(object.top)];
             break;
+        case('i-text'):
+            objectArray = ['i-text', Math.floor(object.left), Math.floor(object.top), Math.floor(object.width), Math.floor(object.height), object.fontSize, object.text];
+            break;
+        case('text'):
+            objectArray = ['text', Math.floor(object.left), Math.floor(object.top), Math.floor(object.width), Math.floor(object.height), object.fontSize, object.text];
+            break;
+        case('textbox'):
+            objectArray = ['textbox', Math.floor(object.left), Math.floor(object.top), Math.floor(object.width), Math.floor(object.height), object.fontSize, object.text];
+            break;
         default:
 
             break;
     }
     return objectArray;
+}
+
+
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+function showTextControls(object) {
+    if (!elemendAdded) {
+        var div = document.createElement("div");
+        div.style.width = "500px";
+        div.style.color = "white";
+        div.style.margin = "10px auto"
+
+        var button = document.createElement("button");
+        button.class = "donkeyButtons";
+
+
+        div.innerHTML = "<button class='controlButtons'  onclick='increaseTextSize()'>" +
+            " + </button>" +
+            "<span>text size</span>" +
+            "<button class='controlButtons'  onclick='decreaseTextSize()'>" +
+            " - </button>";
+        document.getElementById('elementSettingsDiv').appendChild(div);
+    }
+}
+
+function increaseTextSize() {
+    var object = donkeyCanvas.getActiveObject();
+    var fontSize = object.get('fontSize');
+    fontSize += 1;
+    object.set('fontSize', fontSize);
+    donkeyCanvas.renderAll();
+}
+
+function decreaseTextSize(object) {
+    var object = donkeyCanvas.getActiveObject();
+    var fontSize = object.get('fontSize');
+    fontSize -= 1;
+    object.set('fontSize', fontSize);
+    donkeyCanvas.renderAll();
 }
